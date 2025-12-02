a792cc616bf917030343bbb57e17c86f3d40b179
SaidosHits <saidldik937@gmail.com>
2025-12-02 23:37:47 +0100

    Update animation.js

--- script/animation.js
+++ script/animation.js
@@ -78,6 +78,7 @@ inView(elements.Star_svg, () => {
 
 // Load Lottie animations
 
+
 loadLottieAnimation({ container: elements.Path, path: './Fox-Bird/lottie/Path.json' });
 loadLottieAnimation({ container: elements.feature1, path: './Fox-Bird/lottie/feature_1.json' });
 loadLottieAnimation({ container: elements.feature2, path: './Fox-Bird/lottie/feature_2.json' });
@@ -85,6 +86,7 @@ loadLottieAnimation({ container: elements.feature3, path: './Fox-Bird/lottie/fea
 loadLottieAnimation({ container: elements.heroImage, path: './Fox-Bird/lottie/Hero image_1.json' });
 loadLottieAnimation({ container: elements.T_image, path: './Fox-Bird/lottie/Team_work.json' });
 loadLottieAnimation({ container: elements.arrow, path: './Fox-Bird/lottie/arrow.json' });
+
 
 
 
